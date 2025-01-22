e8e6a287db27f173b42e43ed9d873aeaecae3fae
Tanguy Damart <tanguy.damart@protonmail.com>
2025-01-22 09:37:05 +0100

    Update

---
+++ run_parameter_search_MPI_test.py
@@ -0,0 +1,13 @@
+from mozaik.meta_workflow.parameter_search import CombinationParameterSearch, SlurmSequentialBackend
+
+CombinationParameterSearch(
+    SlurmSequentialBackend(
+        num_threads=1,
+        num_mpi=16,
+        slurm_options=['--hint=nomultithread', '-N 1-1', '--exclude=w1,w2,w3,w4,w5,w6,w7,w8,w10,w12,w16'],
+        path_to_mozaik_env='[PATH_TO_ENV]'
+    ),      
+    {
+        "pynn_seed": [1],
+    }
+).run_parameter_search()
