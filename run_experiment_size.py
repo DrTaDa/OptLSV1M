981d1ae0c1a828cd10033fede6dba73793e6dace
Tanguy Damart <tanguy.damart@protonmail.com>
2025-01-27 08:53:33 +0100

    Update

--- run_experiment_size.py
+++ run_experiment_size.py
@@ -20,7 +20,7 @@ def create_experiments_stc(model):
 
         # Size Tuning
         MeasureSizeTuning(model, ParameterSet(
-            {'num_sizes': 12, 'max_size': 5.0, 'log_spacing': True, 'orientations': [0],
+            {'num_sizes': 16, 'max_size': 5.0, 'log_spacing': False, 'orientations': [0],
              'positions': [(0,0)], 'spatial_frequency': 0.8, 'temporal_frequency': 2,
              'grating_duration': 2*143*7, 'contrasts': [10, 100], 'num_trials': 2, 'shuffle_stimuli': True
             })),
